cbc6cbbecb1ff832e2c6ad6d30e4310862afa4b9
ksjgusdh123 <122209567+ksjgusdh123@users.noreply.github.com>
2025-06-27 22:58:37 +0900

    player C++로 변환 중

---
+++ GravityRacing/Source/GravityRacing/GRPlayer.cpp
@@ -0,0 +1,136 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GRPlayer.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+AGRPlayer::AGRPlayer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 300.f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+	Camera->bUsePawnControlRotation = false;
+
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Game/Assets/Player/Mesh/Skin_1/SK_Skin_1.SK_Skin_1"));
+
+	if (MeshAsset.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(MeshAsset.Object);
+		GetMesh()->SetRelativeLocation(FVector(0.f, 0.f, -90.f));
+		GetMesh()->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
+	}
+}
+
+// Called when the game starts or when spawned
+void AGRPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (APlayerController* controller = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(controller->GetLocalPlayer()))
+		{
+			subsystem->AddMappingContext(IMC, 0);
+		}
+	}
+}
+
+// Called every frame
+void AGRPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AGRPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* input = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AGRPlayer::Move);
+		input->BindAction(LookAction, ETriggerEvent::Triggered, this, &AGRPlayer::Look);
+		input->BindAction(JumpAction, ETriggerEvent::Started, this, &AGRPlayer::JumpStart);
+		input->BindAction(JumpAction, ETriggerEvent::Ongoing, this, &AGRPlayer::JumpStop);
+		input->BindAction(FlipAction, ETriggerEvent::Started, this, &AGRPlayer::Flip);
+	}
+
+}
+
+void AGRPlayer::Landed(const FHitResult& Hit)
+{
+	auto* movement = GetCharacterMovement();
+	movement->SetMovementMode(EMovementMode::MOVE_Walking);
+	movement->bOrientRotationToMovement = true;
+	IsFlip = false;
+}
+
+void AGRPlayer::Move(const FInputActionValue& value)
+{
+	if (Controller == nullptr)
+		return;
+
+	FVector2D input = value.Get<FVector2D>();
+
+	FRotator rot = Controller->GetControlRotation();
+	FRotator yaw(0.f, rot.Yaw, 0.f);
+
+	FVector forward = FRotationMatrix(yaw).GetUnitAxis(EAxis::X);
+	FVector right = FRotationMatrix(yaw).GetUnitAxis(EAxis::Y);
+
+	AddMovementInput(forward, input.Y);
+	AddMovementInput(right, input.X);
+}
+
+void AGRPlayer::Look(const FInputActionValue& value)
+{
+	if (Controller == nullptr)
+		return;
+
+	FVector2D input = value.Get<FVector2D>();
+
+	AddControllerYawInput(input.X);
+	AddControllerPitchInput(input.Y);
+}
+
+void AGRPlayer::JumpStart(const FInputActionValue& value)
+{
+	Jump();
+}
+
+void AGRPlayer::JumpStop(const FInputActionValue& value)
+{
+	StopJumping();
+}
+
+void AGRPlayer::Flip(const FInputActionValue& value)
+{
+	auto* movement = GetCharacterMovement();
+	movement->SetMovementMode(EMovementMode::MOVE_Falling);
+
+	movement->GravityScale = movement->GravityScale * -1;
+
+	movement->bOrientRotationToMovement = false;
+	movement->Velocity = FVector(0.f, 0.f, 0.f);
+
+	FRotator ReverseRot = GetActorRotation();
+	ReverseRot.Roll += 180.f;
+	SetActorRotation(FRotator(ReverseRot));
+
+	IsFlip = true;
+}
+
---
+++ GravityRacing/Source/GravityRacing/GRPlayer.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GRPlayer.generated.h"
+
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS()
+class GRAVITYRACING_API AGRPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AGRPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void Landed(const FHitResult& Hit) override;
+public:
+	void Move(const FInputActionValue& value);
+	void Look(const FInputActionValue& value);
+	void JumpStart(const FInputActionValue& value);
+	void JumpStop(const FInputActionValue& value);
+	void Flip(const FInputActionValue& value);
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<class UInputMappingContext> IMC;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> FlipAction;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	class UCameraComponent* Camera;
+
+private:
+	bool IsFlip{ false };
+};

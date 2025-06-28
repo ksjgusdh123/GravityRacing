176d45163792421c1a6718393acd00fcde92f6d3
sujissi <120546504+sujissi@users.noreply.github.com>
2025-06-29 05:58:30 +0900

    언리얼클라 서버 연결

---
+++ GravityRacing/Source/GravityRacing/GRGameInstance.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GRGameInstance.h"
+
+void UGRGameInstance::Init()
+{
+	Super::Init();
+}
+
+void UGRGameInstance::Shutdown()
+{
+	Super::Shutdown();
+}

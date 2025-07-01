36ea042792cb89e6d1093af9d28adbe29ad630bd
sujissi <120546504+sujissi@users.noreply.github.com>
2025-07-01 17:23:50 +0900

    경로 정리 코인 추가

---
+++ GravityRacing/Source/GravityRacing/Objects/Coin/GRCoin.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Objects/Coin/GRCoin.h"
+#include "GravityRacing.h"  
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Characters/Player/GRPlayer.h" 
+
+AGRCoin::AGRCoin()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+	RootComponent = Collision;
+	Collision->SetSphereRadius(50.f);
+	Collision->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AGRCoin::BeginPlay()
+{
+	Super::BeginPlay();
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &AGRCoin::OnOverlapBegin);
+}
+
+void AGRCoin::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (AGRPlayer* Player = Cast<AGRPlayer>(OtherActor))
+	{
+		GRLOG("Coin!");
+		Destroy();
+	}
+}
